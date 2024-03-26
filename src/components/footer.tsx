6ee7a8290986643b27e2a73263d9876b3b0183d2
peter <ajtwls0205@naver.com>
2024-03-26 18:13:07 +0900

    fixed build error : ' -> &apos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className={FooterStyle.global}>
       <p className={FooterStyle.copyright}>
-        &copy; <span></span> Goomba's github blog. All Rights Reserved.
+        &copy; <span></span> Goomba&apos;s github blog. All Rights Reserved.
       </p>
     </footer>
   );
