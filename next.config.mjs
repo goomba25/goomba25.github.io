30969a074625a5f5445063186b50d5533731fbf6
peter <ajtwls0205@naver.com>
2024-03-26 18:02:21 +0900

    [PAGE] github action test

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  compiler: {
-    styledComponents: {
-      ssr: true,
-      displayName: true,
-      pure: true,
-    },
-  },
 };
 
 export default nextConfig;
