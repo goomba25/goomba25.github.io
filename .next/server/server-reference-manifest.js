5f1776807da6d6de88b18d6a29736f87d6f5e396
peter <ajtwls0205@naver.com>
2024-08-08 00:05:24 +0900

    [PAGE] Fixed build error
    [Description]
    - Fixed config file

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jJdmB9EDqGhtzmy5om6hyHkjQdjALtax1EJo/9tWEu8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jWx8VC+0LCkkDYFjIZvbgEhYKgeOz1kiR4l4B1RCQEs=\"\n}"
