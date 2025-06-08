3c45a405683bf30549aa197c6ce6bcc731739add
peter <ajtwls0205@naver.com>
2025-06-08 20:03:32 +0900

    [PAGE] OPTEE Image for raspberry pi 3
    [Description]
    - Added post to build the rpi3 image
    - Added optee category

--- src/_lib/categoryLists.ts
+++ src/_lib/categoryLists.ts
@@ -1,17 +1,21 @@
 export const CategoryLists = [
   {
-    title: 'C Family',
+    title: "C Family",
     items: [
-      { name: 'C 언어', slug: 'C', displayName: 'C 언어'},
-      { name: 'C++ 언어', slug: 'Cpp', displayName: 'C++ 언어'},
-      { name: 'STL', slug: 'STL', displayName: 'Standard Template Library'}
-    ]
+      { name: "C 언어", slug: "C", displayName: "C 언어" },
+      { name: "C++ 언어", slug: "Cpp", displayName: "C++ 언어" },
+      { name: "STL", slug: "STL", displayName: "Standard Template Library" },
+    ],
   },
   {
-    title: 'Tools',
+    title: "Tools",
     items: [
-      { name: 'Docker', slug: 'Docker', displayName: 'Docker'},
-      { name: 'Git', slug: 'Git', displayName: 'Git'}
-    ]
-  }
-]
+      { name: "Docker", slug: "Docker", displayName: "Docker" },
+      { name: "Git", slug: "Git", displayName: "Git" },
+    ],
+  },
+  {
+    title: "ETC.",
+    items: [{ name: "OPTEE", slug: "optee", displayName: "OPTEE Environment" }],
+  },
+];
