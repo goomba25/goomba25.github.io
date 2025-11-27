db002cb8ce5d2a350c7801c5f9f494bfe0df43c0
goomba25 <66657132+goomba25@users.noreply.github.com>
2025-11-27 16:40:06 +0000

    Deploying to gh-pages from @ goomba25/goomba25.github.io@3bd59649ba037bf3fa4f12888cd7ba2b2b993fe9 🚀

---
+++ _next/static/Iv3MARk-QufM90zJROdc4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Farchive\u002F[category]","\u002Farchive\u002F[category]\u002F[slug]","\u002Farchive\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
